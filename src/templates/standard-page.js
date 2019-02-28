08bd9dea3197ddf2f6ff728105b9b255182417ee
[Dan Farrow] <dan@zellement.com>
2019-02-28 20:26:16 +0000

    Page layout

--- src/templates/standard-page.js
+++ src/templates/standard-page.js
@@ -9,10 +9,12 @@ export const StandardPageTemplate = ({ title, content, contentComponent }) => {
 
   return (
     <section>
-      <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-        {title}
-      </h2>
-      <PageContent className="content" content={content} />
+      <div className="container">
+        <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+          {title}
+        </h2>
+        <PageContent className="content" content={content} />
+      </div>
     </section>
   )
 }
