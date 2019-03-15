1327680b6756139d4f012d07a2fd75f5cc48b169
Dan Farrow <dan@zellement.com>
2019-03-15 22:01:44 +0000

    Fix Netlify error - graphQL looking up non existent field

--- src/components/GigsUpcoming.js
+++ src/components/GigsUpcoming.js
@@ -22,7 +22,6 @@ const GigsUpcoming = () => (
                   date
                   location
                   datetime
-                  details
                 }
               }
             }
